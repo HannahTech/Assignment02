7b3a5ec672f4c7e41c831f58126effe5ba91c6f7
IamHeny <hkhajehp@my.centennialcollege.ca>
2021-10-28 05:58:40 -0400

    Updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contact"
+    //"URI": "mongodb://localhost/business_contact"
+    "URI":"mongodb+srv://IamHeny:Ah8102211@hengamehserver.aby60.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
